6da579a2d1e9df2c62a59aa6c910d5edf2bed45e
DouglasB834 <douglas_borges09@hotmail.com>
2022-08-23 20:12:05 -0300

    feat/add listar cliente

---
+++ src/js/resp.js
@@ -0,0 +1,12 @@
+import { Api } from "./api.js"
+import { CriarCardClient } from "./renderizarCliente.js";
+
+
+// const listaBtn = document.querySelector(".listaBtn");
+
+const dados = await Api.listarClientes()
+CriarCardClient.card(dados)
+// listaBtn.addEventListener("click", async()=>{
+    
+
+// })
